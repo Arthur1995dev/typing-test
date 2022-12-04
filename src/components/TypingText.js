5bafdc2bbc593ef55d78994600c2827ff1b0209f
Arthur1995dev <h1f728jri3@yandex.ru>
2022-12-04 12:48:53 +0400

    Добавил адаптив, кнопку рестарта

--- src/components/TypingText.js
+++ src/components/TypingText.js
@@ -17,13 +17,20 @@ function TypingText() {
         dispatch({type: 'SET_RESET'})
     }
     return (
-        <div className='typing-text_container'>
+        <div className='typing-test_container'>
             <div className="current-info">
                 <p className="speed">Cкорость <br/>{state.speed} зн/мин.</p>
                 <p className="accuracy">Точность <br/>{state.accuracy}%</p>
             </div>
-            <div className="text-area">{text}</div>
-            <div className="reset_container" onClick={restartHundler}><img src={restart} alt='restart' width={25} height={25}></img></div>
+            <div className="text-area_container">
+                <p className="text-area">{text}</p>
+                <div className="reset_container" onClick={restartHundler}>
+                    <div className="resetBtn"><i className="fa-solid fa-repeat"></i></div>
+                    <div className="progress_bar" style={{
+                        background: `linear-gradient(to top, green ${(state.charIndex + 1) / state.text.length * 100}%, 
+                        white ${(state.charIndex + 1) / state.text.length * 100}%)`}}></div>
+                </div>
+            </div>
         </div>
     )
 }
